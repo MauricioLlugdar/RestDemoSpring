a416e3c62457ecf3f0598db37f9177a3e397f252
Maurillugdar <maurillugdar@gmail.com>
2025-07-25 19:21:48 -0300

    Spring Initializr

---
+++ src/main/java/com/maullu/maullu/MaulluApplication.java
@@ -0,0 +1,13 @@
+package com.maullu.maullu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaulluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaulluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maullu/maullu/MaulluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maullu.maullu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaulluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
